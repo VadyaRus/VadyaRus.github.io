55bebf3e33cdf62a8ac383f9cfeaaed66f2c6710
Vadim Maltsev <vadyarus@gmail.com>
2022-06-02 09:59:27 -0400

    Wordart section is now styled. Skills section is now styled. Skills section is now reactive to browser window size.

--- scripts/wordart.js
+++ scripts/wordart.js
@@ -10,12 +10,27 @@ anychart.onDocumentReady(function() {
         chart.textSpacing(5);
 
         // set the mode of the tag cloud
-        chart.mode("rect");
+        //chart.mode("rect");
 
         chart.tooltip(false);
 
         // Display the word cloud chart
         chart.container('container');
+
+        // Configure visual settings of the chart
+        chart.normal().fontSize(18);        
+        chart.normal().fill("#fff", 0.5);
+        chart.normal().fontStyle('normal')
+
+        chart.hovered().fontSize(22);
+        chart.hovered().fill("#fff", 0.5);
+        //chart.hovered().stroke("#303030", 0.1);
+        chart.hovered().fontStyle('bold')
+
+        // Set background to be completely transparent
+        var background = chart.background();        
+        background.fill('#FFF', 0.0);
+
         chart.draw();
 
         elements = document.getElementsByClassName("anychart-credits");
